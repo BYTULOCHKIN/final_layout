990f5b82fec9ea0c9ab5b88bcc940985ae79226d
BYTULOCHKIN <69384983+BYTULOCHKIN@users.noreply.github.com>
2020-12-14 15:25:39 +0200

    update layout

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
-let progress = document.getElementById("progressbar"),
-  totalHight = document.body.scrollHeight - window.innerHeight;
-window.onscroll = function () {
-  let progressHeight = (window.pageYOffset / totalHight) * 100;
-  progress.style.height = progressHeight + "%";
-};
+window.addEventListener("DOMContentLoaded", () => {
+  let progress = document.getElementById("progressbar"),
+    totalHight = document.body.scrollHeight - window.innerHeight;
+  window.onscroll = function () {
+    let progressHeight = (window.pageYOffset / totalHight) * 100;
+    progress.style.height = progressHeight + "%";
+  };
 
-
+  new Swiper(".swiper-container", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    // Отступы между слайдами
+    spaceBetween: 19,
+    // Бесконечный слайдер
+    loop: false,
+    //Ширина экрана. Количество слайдов для показа
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      1100: {
+        slidesPerView: 2,
+      },
+      1640: {
+        slidesPerView: 3,
+      },
+    },
+  });
+});
